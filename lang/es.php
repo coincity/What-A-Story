8e76f58384b4072b8c635a2be193a7ab12ff53c5
coincity <coincity@users.noreply.github.com>
2022-04-29 07:38:27 +0200

    Create es.php
    Spanish translate

---
+++ lang/es.php
@@ -0,0 +1,12 @@
+<?php
+$l = array(
+  "index_Title" => "Titulo",
+  "index_Your_Name" => "Tu Nombre",
+  "index_Your_Story" => "Tu Historia...",
+  "index_Publish" => "Publicar",
+  "index_Ajax_error" => "Ajax error...",
+
+  "offline_Title" => "Sitio fuera de linea...",
+  "offline_Message" => "Volveremos pronto!"
+);
+?>
